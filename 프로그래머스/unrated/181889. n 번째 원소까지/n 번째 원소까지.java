24e297ec35878367a1826414339d61a41f8a1c26
SEOIN KIM <96633718+seoinee@users.noreply.github.com>
2023-07-24 17:06:13 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        answer = new int[n];
+        int index = 0;
+        
+        for(int i = 0; i < n; i++) {
+            answer[index++] = num_list[i];
+        }
+        return answer;
+    }
+}
