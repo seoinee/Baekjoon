3400bdaf87a565b5a3fae65d14688516de99fe0b
SEOIN KIM <96633718+seoinee@users.noreply.github.com>
2024-01-29 12:15:48 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int even = 0;
+        int odd = 0;
+        int k = 0;
+        int m = 0;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 != 0) {
+                odd = odd * 10 + num_list[i];
+            } else {
+                even = even * 10 + num_list[i];
+            }
+        }
+        answer = odd + even;
+        return answer;
+    }
+}
