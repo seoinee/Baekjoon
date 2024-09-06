0495ced52c129e036ff60b7f0466ce4787b8c9c8
SEOIN KIM <96633718+seoinee@users.noreply.github.com>
2024-09-06 20:52:10 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.17 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -0,0 +1,9 @@
+def solution(my_string, s, e):
+    answer = ''
+    for i in range(0, s):
+        answer += my_string[i]
+    for i in range(e, s - 1, -1):
+        answer += my_string[i]
+    for i in range(e + 1, len(my_string)):
+        answer += my_string[i]
+    return answer
