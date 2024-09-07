08ee8ac01e90391f136ce9a33ec6e399925a2867
SEOIN KIM <96633718+seoinee@users.noreply.github.com>
2024-09-07 10:57:18 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,8 @@
+def solution(my_string, overwrite_string, s):
+    answer = ''
+    for i in range(s):
+        answer += my_string[i]
+    answer += overwrite_string
+    for i in range(s + len(overwrite_string), len(my_string)):
+        answer += my_string[i]
+    return answer
