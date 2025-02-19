425b3ff3e4b5428450f1e8faa8f63b1183baeb22
SEOIN KIM <96633718+seoinee@users.noreply.github.com>
2025-02-19 20:18:44 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i = 0; i < queries.length; i++) {
+            int temp = arr[queries[i][1]]; //temp = 3
+            arr[queries[i][1]] = arr[queries[i][0]];
+            arr[queries[i][0]] = temp;
+        }
+        return arr;
+    }
+}
