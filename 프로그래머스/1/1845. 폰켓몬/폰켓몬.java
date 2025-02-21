c58cd0f5f362f498820cb157d1edb1eede4e5543
SEOIN KIM <96633718+seoinee@users.noreply.github.com>
2025-02-21 18:28:37 +0900

    [level 1] Title: 폰켓몬, Time: 5.64 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>(); // 중복 제거, int[] 는 aslist 사용 불가
         
         for(int num : nums) {
             set.add(num);
